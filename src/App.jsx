acbcececeed999a59cb6419d095dc4b71f56b562
Saif <ro1c@yahoo.com>
2025-07-31 18:32:41 +0300

    refactor: Remove SpeedInsights import from App component

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@
 import Home from './pages/Home';
 import About from './pages/About';
 import Settings from './pages/Settings';
-import { SpeedInsights } from "@vercel/speed-insights/react";
 import { Analytics } from '@vercel/analytics/react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { PageNotFound } from './pages/PageNotFound';
